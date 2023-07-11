30b9bd4a6a1fa2d47072d9eefa0ea51b6d4c0053
AnishBade <badeanish6045@gmail.com>
2023-07-11 14:21:54 +0545

    unsuccesful dockerizing attempl

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,3 +145,5 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
 ]
+
+GDAL_LIBRARY_PATH = '/usr/bin/gdal-config'
--- backend/readme.txt
+++ backend/readme.txt
@@ -4,3 +4,7 @@ super user
 username: anish
 email : anish@anish.com
 password: anish
+
+
+
+GDAL_LIBRARY_PATH = '/usr/bin/gdal-config'
